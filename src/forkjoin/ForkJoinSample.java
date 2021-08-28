0f89178b139584740652359d8ef9199b0a252b5c
MARI <mari20201101@gmail.com>
2021-08-28 22:27:20 +0900

    Check fork join logic

--- src/forkjoin/ForkJoinSample.java
+++ src/forkjoin/ForkJoinSample.java
@@ -6,7 +6,8 @@ public class ForkJoinSample {
     private static final ForkJoinPool mainPool = new ForkJoinPool();
     public static void main(String[] args) {
         ForkJoinSample sample = new ForkJoinSample();
-        sample.calculate();
+       // sample.calculate();
+        sample.calculate2();
     }
 
     private void calculate() {
@@ -14,6 +15,14 @@ public class ForkJoinSample {
         long to = 10;
 
         GetSum getSum = new GetSum(from, to);
+        Long result = mainPool.invoke(getSum);
+        System.out.println("Fork join : total sum => " + from + " ~ " + to + " = result : "+result);
+    }
+    private void calculate2() {
+        long from = 0;
+        long to = 10;
+
+        GetSum2 getSum = new GetSum2(from, to);
         Long result = mainPool.invoke(getSum);
         System.out.println("Fork join : total sum => " + from + " ~ " + to + " = result : "+result);
     }
